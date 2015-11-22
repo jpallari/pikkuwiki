9e3c6eba650f49f49fbc7996753b1958e52b1444
Jaakko Pallari <jkpl@lepovirta.org>
2015-11-22 17:16:51 +0000

    Bash completion: improve flag handling for find

--- bash_completion.sh
+++ bash_completion.sh
@@ -11,22 +11,26 @@ __pw_links() {
 
 _pikkuwiki() {
     local cur prev cmd
+    local commands="open find resolve"
+    local flags="-l -p -F"
     COMPREPLY=()
     cur=${COMP_WORDS[COMP_CWORD]}
     prev=${COMP_WORDS[COMP_CWORD-1]}
     cmd=${COMP_WORDS[1]}
 
     if [ $COMP_CWORD -eq 1 ]; then
-        COMPREPLY=($(compgen -W "open find resolve" -- $cur))
+        COMPREPLY=($(compgen -W "$commands" -- $cur))
     elif [[ "$cmd" =~ (o|pen) || "$cmd" =~ (r|resolve) ]]; then
         COMPREPLY=($(compgen -W "$(__pw_links)" -- $cur))
     elif [[ "$cmd" =~ (f|find) ]]; then
         if [[ "$prev" =~ -(l|p|F) ]]; then
             if [ "$prev" = "-l" ]; then
                 COMPREPLY=($(compgen -W "$(__pw_links)" -- $cur))
+            elif [ "$prev" = "-F" ]; then
+                COMPREPLY=($(compgen -W "$flags" -- $cur))
             fi
         else
-            COMPREPLY=($(compgen -W "-l -p -F" -- $cur))
+            COMPREPLY=($(compgen -W "$flags" -- $cur))
         fi
     fi
 }
