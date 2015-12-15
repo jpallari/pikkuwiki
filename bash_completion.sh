c315cae1aabefbb83fcb5149c5cf7f43d94a8625
Jaakko Pallari <jkpl@lepovirta.org>
2015-12-15 15:02:08 +0000

    bash completion: separate flags for find/show, unambiguous parameter matching

--- bash_completion.sh
+++ bash_completion.sh
@@ -4,7 +4,8 @@ _pikkuwiki() {
     local cur prev cmd
     local commands="open find show resolve view"
     local formatters="header link file pretty space"
-    local flags="-l -p -F"
+    local show_flags="-l -p -F"
+    local find_flags="-p -F"
     COMPREPLY=()
     cur=${COMP_WORDS[COMP_CWORD]}
     prev=${COMP_WORDS[COMP_CWORD-1]}
@@ -12,25 +13,25 @@ _pikkuwiki() {
 
     if [ $COMP_CWORD -eq 1 ]; then
         COMPREPLY=($(compgen -W "$commands" -- $cur))
-    elif [[ "$cmd" =~ (o|pen) || "$cmd" =~ (r|resolve) || "$cmd" =~ (v|view) ]]; then
+    elif [[ "$cmd" =~ ^(o|pen)$ || "$cmd" =~ ^(r|resolve)$ || "$cmd" =~ ^(v|view)$ ]]; then
         COMPREPLY=($(compgen -W "$(pikkuwiki find -F space)" -- $cur))
-    elif [[ "$cmd" =~ (f|find) ]]; then
-        if [[ "$prev" =~ -(p|F) ]]; then
+    elif [[ "$cmd" =~ ^(f|find)$ ]]; then
+        if [[ "$prev" =~ ^-(p|F)$ ]]; then
             if [ "$prev" = "-F" ]; then
                 COMPREPLY=($(compgen -W "$formatters" -- $cur))
             fi
         else
-            COMPREPLY=($(compgen -W "$flags" -- $cur))
+            COMPREPLY=($(compgen -W "$find_flags" -- $cur))
         fi
-    elif [[ "$cmd" =~ (s|show) ]]; then
-        if [[ "$prev" =~ -(l|p|F) ]]; then
+    elif [[ "$cmd" =~ ^(s|show)$ ]]; then
+        if [[ "$prev" =~ ^-(l|p|F)$ ]]; then
             if [ "$prev" = "-l" ]; then
                 COMPREPLY=($(compgen -W "$(pikkuwiki find -F space)" -- $cur))
             elif [ "$prev" = "-F" ]; then
                 COMPREPLY=($(compgen -W "$formatters" -- $cur))
             fi
         else
-            COMPREPLY=($(compgen -W "$flags" -- $cur))
+            COMPREPLY=($(compgen -W "$show_flags" -- $cur))
         fi
     fi
 }
